0d23218596a2b86aa8e7a26e5d929f6d13a89ed7
volkodavwar <volkodavwar@mail.ru>
2020-10-04 20:07:46 +0600

    Котоеду не понравилась задача про возраст и поэтому пришлось переделывать

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -68,14 +68,15 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String {
-    return when {
-    age / 10 % 10 == 1 -> "$age лет"
-    age % 10 == 1 -> "$age год"
-    age / 10 % 10 == 9 -> "$age лет"
-    else -> "$age года"
-}
-}
+fun ageDescription(age: Int): String =
+    when (age % 100) {
+        in 11..14 -> "$age лет"
+        else -> when (age % 10) {
+            1 -> "$age год"
+            in 2..4 -> "$age года"
+            else -> "$age лет"
+        }
+    }
 
 /**
  * Простая (2 балла)
