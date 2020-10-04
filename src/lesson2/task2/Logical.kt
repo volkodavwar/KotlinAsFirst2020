8c8d6135e692d1cfc47d801d848449b6f2fb8451
volkodavwar <volkodavwar@mail.ru>
2020-10-04 19:59:38 +0600

    Решил задачу про счастливое число и про круг внутри круга

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,7 +18,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean = if ((number / 1000) + (number / 100) % 10 == (number % 10) + ((number / 10) % 10)) true else false
 
 /**
  * Простая (2 балла)
@@ -48,7 +48,7 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean = if ( (r2 >= r1) && (r2 >= (x2 - x1 + r2) ) && (r2 >= (y2 - y1 + r2) ) ) true else false
 
 /**
  * Средняя (3 балла)
