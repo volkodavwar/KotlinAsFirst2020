facef094334707b577fb83a4c5f93fa25c854a19
volkodavwar <volkodavwar@mail.ru>
2020-10-30 21:22:15 +0300

    434343

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -73,4 +73,8 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    return ((r >= a) && (s >= b)) || ((r >= b) && (s >= a)) || ((r >= a) && (s >= c)) || ((r >= c) &&
+            (s >= a)) || (((r >= c) && (s >= b)) || ((r >= b) && (s >= c)))
+
+}
